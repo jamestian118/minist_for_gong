a72613113c3a6893c157233110217b1cc68248b4
Longshou <1281950904@qq.com>
2023-04-11 23:11:43 +0800

    增加使用无标签数据集的CNN

--- train.py
+++ train.py
@@ -36,80 +36,25 @@ def evaluate(model, data_loader, device, return_accuracy=False):
 
 
 # CNN
-# def train_and_evaluate_CNN(epochs, batch_size, train_size, val_split, learning_rate, device):
-#     train_accuracy_history = []  # 记录训练准确率
-#     val_accuracy_history = []  # 记录验证准确率
-#     x = np.array(range(1, epochs+1))
-#     model = CNN(num_classes=10).to(device)
-#
-#     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-#     criterion = torch.nn.CrossEntropyLoss()  # 输出仍然采用交叉熵损失函数，不过在内部使用了softmax函数
-#
-#     print(f"Training with {train_size * 100}% of the training data")
-#     train_loader, val_loader, test_loader, unlabeled_loader = load_mnist_data(batch_size, train_size, val_split)
-#     # train过程
-#     model.train()
-#
-#     for epoch in range(epochs):
-#         running_loss = 0.0
-#         total = 0
-#         correct = 0
-#
-#         for batch_idx, (data, target) in enumerate(train_loader):
-#             data, target = data.to(device), target.to(device)
-#             optimizer.zero_grad()
-#             output = model(data)
-#             loss = criterion(output, target)
-#             loss.backward()
-#             optimizer.step()
-#
-#             running_loss += loss.item()
-#             _, predicted = torch.max(output.data, 1)
-#             total += target.size(0)
-#             correct += (predicted == target).sum().item()
-#
-#         train_accuracy = correct / total
-#         train_accuracy_history.append(train_accuracy)
-#         val_accuracy = evaluate(model, val_loader, device, return_accuracy=True)
-#         val_accuracy_history.append(val_accuracy)
-#         print(f'Epoch {epoch + 1}/{epochs}, Loss: {running_loss / (batch_idx + 1)}, '
-#               f'Train Accuracy: {train_accuracy:.2%}, Val Accuracy: {val_accuracy:.2%}')
-#     evaluate(model, test_loader, device)
-#
-#     # 历次准确率绘制
-#     plt.plot(x,train_accuracy_history, label="Train Accuracy")
-#     plt.plot(x,val_accuracy_history, label="Val Accuracy")
-#     plt.xlabel("Epoch")
-#     plt.ylabel("Accuracy")
-#     plt.legend()
-#     plt.scatter(x, train_accuracy_history, marker='o')
-#     plt.scatter(x, val_accuracy_history, marker='o')
-#     plt.scatter(1, test_accuracy, marker='o')
-#     plt.show()
-
-
-# FCNN
-
 def train_and_evaluate_CNN(epochs, batch_size, train_size, val_split, learning_rate, device):
-    train_accuracy_history = []
-    val_accuracy_history = []
+    train_accuracy_history = []  # 记录训练准确率
+    val_accuracy_history = []  # 记录验证准确率
     x = np.array(range(1, epochs+1))
-
     model = CNN(num_classes=10).to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss()  # 输出仍然采用交叉熵损失函数，不过在内部使用了softmax函数
 
     print(f"Training with {train_size * 100}% of the training data")
     train_loader, val_loader, test_loader, unlabeled_loader = load_mnist_data(batch_size, train_size, val_split)
+    # train过程
+    model.train()
 
     for epoch in range(epochs):
         running_loss = 0.0
         total = 0
         correct = 0
 
-        # labeled data
-        model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
@@ -125,23 +70,13 @@ def train_and_evaluate_CNN(epochs, batch_size, train_size, val_split, learning_r
 
         train_accuracy = correct / total
         train_accuracy_history.append(train_accuracy)
-
         val_accuracy = evaluate(model, val_loader, device, return_accuracy=True)
         val_accuracy_history.append(val_accuracy)
-
-        # unlabeled data
-        model.train()
-        for batch_idx, (data, _) in enumerate(unlabeled_loader):
-            data = data.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = -torch.mean(torch.sum(F.log_softmax(output, dim=1) * torch.softmax(output, dim=1), dim=1))
-            loss.backward()
-            optimizer.step()
-
-        print(f'Epoch {epoch + 1}/{epochs}, Loss: {running_loss / (batch_idx + 1)}, Train Accuracy: {train_accuracy:.2%}, Val Accuracy: {val_accuracy:.2%}')
-
+        print(f'Epoch {epoch + 1}/{epochs}, Loss: {running_loss / (batch_idx + 1)}, '
+              f'Train Accuracy: {train_accuracy:.2%}, Val Accuracy: {val_accuracy:.2%}')
     evaluate(model, test_loader, device)
+
+    # 历次准确率绘制
     plt.plot(x,train_accuracy_history, label="Train Accuracy")
     plt.plot(x,val_accuracy_history, label="Val Accuracy")
     plt.xlabel("Epoch")
@@ -151,6 +86,70 @@ def train_and_evaluate_CNN(epochs, batch_size, train_size, val_split, learning_r
     plt.scatter(x, val_accuracy_history, marker='o')
     plt.scatter(1, test_accuracy, marker='o')
     plt.show()
+
+# def train_and_evaluate_CNN(epochs, batch_size, train_size, val_split, learning_rate, device):
+#     train_accuracy_history = []
+#     val_accuracy_history = []
+#     x = np.array(range(1, epochs+1))
+#
+#     model = CNN(num_classes=10).to(device)
+#
+#     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#     criterion = torch.nn.CrossEntropyLoss()
+#
+#     print(f"Training with {train_size * 100}% of the training data")
+#     train_loader, val_loader, test_loader, unlabeled_loader = load_mnist_data(batch_size, train_size, val_split)
+#
+#     for epoch in range(epochs):
+#         running_loss = 0.0
+#         total = 0
+#         correct = 0
+#
+#         # labeled data
+#         model.train()
+#         for batch_idx, (data, target) in enumerate(train_loader):
+#             data, target = data.to(device), target.to(device)
+#             optimizer.zero_grad()
+#             output = model(data)
+#             loss = criterion(output, target)
+#             loss.backward()
+#             optimizer.step()
+#
+#             running_loss += loss.item()
+#             _, predicted = torch.max(output.data, 1)
+#             total += target.size(0)
+#             correct += (predicted == target).sum().item()
+#
+#         train_accuracy = correct / total
+#         train_accuracy_history.append(train_accuracy)
+#
+#         val_accuracy = evaluate(model, val_loader, device, return_accuracy=True)
+#         val_accuracy_history.append(val_accuracy)
+#
+#         # unlabeled data
+#         model.train()
+#         for batch_idx, (data, _) in enumerate(unlabeled_loader):
+#             data = data.to(device)
+#             optimizer.zero_grad()
+#             output = model(data)
+#             loss = -torch.mean(torch.sum(F.log_softmax(output, dim=1) * torch.softmax(output, dim=1), dim=1))
+#             loss.backward()
+#             optimizer.step()
+#
+#         print(f'Epoch {epoch + 1}/{epochs}, Loss: {running_loss / (batch_idx + 1)}, Train Accuracy: {train_accuracy:.2%}, Val Accuracy: {val_accuracy:.2%}')
+#
+#     evaluate(model, test_loader, device)
+#     plt.plot(x,train_accuracy_history, label="Train Accuracy")
+#     plt.plot(x,val_accuracy_history, label="Val Accuracy")
+#     plt.xlabel("Epoch")
+#     plt.ylabel("Accuracy")
+#     plt.legend()
+#     plt.scatter(x, train_accuracy_history, marker='o')
+#     plt.scatter(x, val_accuracy_history, marker='o')
+#     plt.scatter(1, test_accuracy, marker='o')
+#     plt.show()
+
+# FCNN
 
 
 def train_and_evaluate_FCNN(epochs, batch_size, train_size, val_split, hidden_size1, hidden_size2, learning_rate):
@@ -247,4 +246,4 @@ def train_and_evaluate_FCNN(epochs, batch_size, train_size, val_split, hidden_si
 # train_and_evaluate_FCNN(epochs, batch_size, train_size, val_split, hidden_size1,hidden_size2, learning_rate)
 
 # CNN模型参数设置
-train_and_evaluate_CNN(epochs=10, batch_size=64, train_size=0.1, val_split=0.1, learning_rate=0.001, device=device)
+train_and_evaluate_CNN(epochs=10, batch_size=64, train_size=0.3, val_split=0.1, learning_rate=0.001, device=device)
